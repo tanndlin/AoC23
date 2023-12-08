fb16ad794c3742138a09c7c9dbb872b55f16e279
tanndlin <tanndlin@gmail.com>
2023-12-08 16:35:27 -0500

    Day 6

---
+++ solutions/day6.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from util.solution_base import SolutionBase
+
+
+class Solution(SolutionBase):
+    def __init__(self, data, expectedValue=-1):
+        super().__init__(data, expectedValue)
+        self.data[0] = " ".join(data[0].split())
+        self.data[1] = " ".join(data[1].split())
+
+        self.lengths = list(map(int, self.data[0].split(" ")[1:]))
+        self.distances = list(map(int, self.data[1].split(" ")[1:]))
+
+    def part1(self):
+        ways = [0] * len(self.lengths)
+        for race in range(len(self.lengths)):
+            length = self.lengths[race]
+            distance = self.distances[race]
+
+            for speed in range(length):
+                if (length - speed) * speed > distance:
+                    ways[race] += 1
+
+        return np.prod(ways)
+
+    def part2(self):
+        length = int("".join([str(i) for i in self.lengths]))
+        distance = int("".join([str(i) for i in self.distances]))
+
+        ways = 0
+        for speed in range(length):
+            if (length - speed) * speed > distance:
+                ways += 1
+
+        return ways
