31108c686ad753f3da35951b030804b82a78b694
Tanner Sandlin <tanndlin@gmail.com>
2023-12-08 18:42:43 -0500

    Slightly better

--- solutions/day7.py
+++ solutions/day7.py
@@ -4,8 +4,6 @@ import numpy as np
 
 from util.solution_base import SolutionBase
 
-# values = "123456789TJQKA"
-
 
 class Solution(SolutionBase):
     def preprocess(self):
@@ -13,10 +11,11 @@ class Solution(SolutionBase):
             self.data[i] = self.data[i].split(" ")
             self.data[i] = {"cards": self.data[i][0], "bid": int(self.data[i][1])}
 
+    @functools.lru_cache(maxsize=None)
     def getWinType(self, hand):
-        freq = np.zeros(14)
+        freq = np.zeros(13)
         for card in hand:
-            freq[self.values.index(card[0])] += 1
+            freq[self.values[card]] += 1
 
         if 5 in freq:
             return 6
@@ -40,7 +39,7 @@ class Solution(SolutionBase):
 
         return 0
 
-    def evalCards(self, cards1, cards2):
+    def evalCards(self, cards1, cards2, breakTie=True):
         winType1 = self.getWinType(cards1)
         winType2 = self.getWinType(cards2)
 
@@ -49,11 +48,14 @@ class Solution(SolutionBase):
         elif winType1 > winType2:
             return -1
 
+        if not breakTie:
+            return 0
+
         # same win type
         # Go left to right, higher card wins
         for i in range(5):
-            cardValue1 = self.values.index(cards1[i])
-            cardValue2 = self.values.index(cards2[i])
+            cardValue1 = self.values[cards1[i]]
+            cardValue2 = self.values[cards2[i]]
             if cardValue1 < cardValue2:
                 return 1
             elif cardValue1 > cardValue2:
@@ -67,36 +69,50 @@ class Solution(SolutionBase):
         cards2 = hand2["cards"]
 
         evalCardsResult = 0
-        if self.wildCard:
-            evalCardsResult = self.evalCards(
-                self.makeStrongestHand(cards1), self.makeStrongestHand(cards2)
-            )
-        else:
-            evalCardsResult = self.evalCards(cards1, cards2)
+
+        cardToEval1 = cards1 if not self.wildCard else self.makeStrongestHand(cards1)
+        cardToEval2 = cards2 if not self.wildCard else self.makeStrongestHand(cards2)
+
+        print("to eval")
+        print(cardToEval1, cardToEval2)
+
+        evalCardsResult = self.evalCards(cardToEval1, cardToEval2, breakTie=False)
         if evalCardsResult != 0:
+            print("WINNER", evalCardsResult)
+            print(cards1, cards2)
+
             return evalCardsResult
 
+        print("TIE")
         # Go left to right, higher card wins
+        # Using the original cards, instead of the Jokers
         for i in range(5):
-            cardValue1 = self.values.index(cards1[i])
-            cardValue2 = self.values.index(cards2[i])
+            cardValue1 = self.values[cards1[i]]
+            cardValue2 = self.values[cards2[i]]
             if cardValue1 < cardValue2:
+                print(1)
                 return 1
             elif cardValue1 > cardValue2:
+                print(-1)
                 return -1
+
+        print(0)
 
         return 0
 
-    def part1(self):
-        self.wildCard = False
-        self.values = "123456789TJQKA"
-        sortedHands = sorted(self.data, key=functools.cmp_to_key(self.evalHands))
-
+    def getFinalScore(self, hands):
         s = 0
-        for i, hand in enumerate(reversed(sortedHands)):
+        for i, hand in enumerate(reversed(hands)):
             s += hand["bid"] * (i + 1)
 
         return s
+
+    def part1(self):
+        self.wildCard = False
+        self.values = {k: i for i, k in enumerate("23456789TJQKA")}
+        sortedHands = sorted(self.data, key=functools.cmp_to_key(self.evalHands))
+
+        return self.getFinalScore(sortedHands)
 
     def getAllPossibleHands(self, cards):
         if "J" not in cards:
@@ -123,13 +139,14 @@ class Solution(SolutionBase):
         return min(hands, key=functools.cmp_to_key(self.evalCards))
 
     def part2(self):
-        self.values = "J123456789TQKA"
+        self.values = {k: i for i, k in enumerate("J23456789TQKA")}
         self.wildCard = True
         sortedHands = sorted(self.data, key=functools.cmp_to_key(self.evalHands))
+        for i in sortedHands:
+            print(i)
 
-        s = 0
-        for i, hand in enumerate(reversed(sortedHands)):
-            bid = hand["bid"]
-            s += bid * (i + 1)
+        print(self.getWinType("2JJJJ"))
+        print(self.getWinType("JAAAA"))
+        print(self.evalHands({"cards": "2JJJJ"}, {"cards": "JAAAA"}))
 
-        return s
+        return self.getFinalScore(sortedHands)
